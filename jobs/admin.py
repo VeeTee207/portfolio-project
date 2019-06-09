3344df48a6069b330c96f5fb73ccffe8a8e91fb6
vterehov207 <v.terehov207@gmail.com>
2019-06-09 06:22:17 -0400

    Jobs app up and running

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Job
+# from .models import JobsConfig
+
+
+# admin.site.register(JobsConfig)
+admin.site.register(Job)
