89a9fe8f766ef0b1c93fb8687e86dfa70b8ae0f2
vterehov207 <v.terehov207@gmail.com>
2019-06-16 08:57:13 -0400

    Added bootstrap, resume and blog and all thier pages showing up. ready to cloud up git add -A!

--- blog/models.py
+++ blog/models.py
@@ -20,3 +20,14 @@ class Blog(models.Model):
     pub_date = models.DateTimeField()
     blog_text = models.TextField()
     image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title[:50]
+
+    def summary(self):
+        return self.blog_text[:100]
+
+    def pub_date_pretty(self):
+        # МPython's strftime directivesб % b - month,   http: // strftime.org/
+
+        return self.pub_date.strftime('%b %e,   %y')
